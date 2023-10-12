95ce9cd03ae937b2faf7253628e802b13f7cbd5b
Joel <joelsud1803@gmail.com>
2023-10-12 20:28:51 +0100

    milestone_4.py updated with docstrings

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,20 @@ import random
 
 class Hangman:
 
+    '''
+    This class is used to define the game hangman.
+
+    Attributes:
+        word_list (list): The list of words the computer selects a word at random from.
+        num_lives (int): The number of lives the user starts with.
+    '''
+
     def __init__(self, word_list, num_lives = 5):
+
+        '''
+        See help(Hangman) for accurate signature.
+        '''
+
         self.word_list = word_list
         self.num_lives = num_lives
 
@@ -12,8 +25,15 @@ class Hangman:
         self.list_of_guesses = []
         self.total_num_guesses = 0
 
-    def check_guess(self, guess): # This function checks if a character is in the selected word.
+    def check_guess(self, guess):
         
+        '''
+        This method is used to check whether the letter guessed by the user is in the selected word. if not then it takes a life away from the user.
+
+        Args:
+            guess (string): This is the letter input from the user.
+        '''
+
         guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -28,7 +48,16 @@ class Hangman:
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} left!")
 
-    def ask_for_input(self): #This function asks for an input, validates it is a single alphabetical character and then calls the check guess function.
+    def ask_for_input(self):
+
+        '''
+        This method asks the user for an input and checks that the letter is:
+            1. a single character
+            2. alphabetical
+            3. not been guessed before
+        if the input is valid then it passes it as an argument into the check_guess() method.
+        '''
+
         while True:
             guess = input("Please enter a single letter: ")
             validate_guess_is_alphabetical = guess.isalpha()
